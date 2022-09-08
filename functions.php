7a23bcba7dc92b36a6d5293de6560225878c4fe0
Giovanni Ova <giovanni.alias.ova@gmail.com>
2022-09-08 12:19:28 +0000

    use  instead of WP_QUERY

--- functions.php
+++ functions.php
@@ -1,24 +1,25 @@
 <?php
 const _RUN_MODE_ = "production";
 const _API_TOKEN_ = "api_token";
-const _API_BASE_URL_ = (_RUN_MODE_ === "development" ? "http://localhost:3000":"https://notaire-tsakadi.tg")."/wordpress-caching";
+const _API_BASE_URL_ = (_RUN_MODE_ === "development" ? "http://localhost:3000" : "https://notaire-tsakadi.tg") . "/wordpress-caching";
 const _AVAILABLE_POST_TYPES_ = ["post", "page"];
 
-function excite(string $_endpoint, array $_data=[]) {
-  $data = json_encode(array_merge(['token' => _API_TOKEN_], $_data));
-  $curl = curl_init();
-  curl_setopt($curl, CURLOPT_URL, _API_BASE_URL_.$_endpoint);
-  curl_setopt($curl, CURLOPT_HTTPHEADER, [
-    "Content-Type: application/json"
-  ]);
-  curl_setopt($curl, CURLOPT_POST, 1);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+function excite(string $_endpoint, array $_data = [])
+{
+    $data = json_encode(array_merge(['token' => _API_TOKEN_], $_data));
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, _API_BASE_URL_ . $_endpoint);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, [
+        "Content-Type: application/json"
+    ]);
+    curl_setopt($curl, CURLOPT_POST, 1);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-  $result = curl_exec($curl);
-  curl_close($curl);
+    $result = curl_exec($curl);
+    curl_close($curl);
 
-  return $result;
+    return $result;
 }
 
 // { Theme supports }
@@ -28,12 +29,13 @@ add_theme_support("title-tag");
 
 // { Actions }
 
-function post_saved($_id, $_post, $_is_update) {
-    if(!array_search($_post->post_type, _AVAILABLE_POST_TYPES_)) return;
+function post_saved($_id, $_post, $_is_update)
+{
+    if (!array_search($_post->post_type, _AVAILABLE_POST_TYPES_)) return;
 
-    $status = $_post->post_status === "publish" ? "published":"not_published";
+    $status = $_post->post_status === "publish" ? "published" : "not_published";
 
-    $endpoint = "/". ($_is_update ? "update":"add");
+    $endpoint = "/" . ($_is_update ? "update" : "add");
     $data = [
         'post_type' => $_post->post_type,
         'id' => $_id,
@@ -41,7 +43,7 @@ function post_saved($_id, $_post, $_is_update) {
         'status' => $status
     ];
 
-    if($_post->post_type !== "page") {
+    if ($_post->post_type !== "page") {
         $data["title"] = $_post->post_title;
         $data["_excerpt"] = $_post->post_excerpt;
     }
@@ -49,19 +51,20 @@ function post_saved($_id, $_post, $_is_update) {
     excite($endpoint, $data);
 }
 
-function post_deleted($_id, $_post) {
+function post_deleted($_id, $_post)
+{
 
-  if($_post->post_type !== "post") {
-    return;
-  }
-  
-  $endpoint = "/delete";
-  $data = [
-    'post_type' => $_post->post_type,
-    'id' => $_id
-  ];
+    if ($_post->post_type !== "post") {
+        return;
+    }
 
-  excite($endpoint, $data);
+    $endpoint = "/delete";
+    $data = [
+        'post_type' => $_post->post_type,
+        'id' => $_id
+    ];
+
+    excite($endpoint, $data);
 }
 
 add_action("save_post", "post_saved", 10, 3);
@@ -76,63 +79,56 @@ const _EMPTY_DATA_ = [
     ]
 ];
 
-function load_articles() {
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Methods: Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-  header("Access-Control-Allow-Headers: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+function load_articles()
+{
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    header("Access-Control-Allow-Headers: GET, POST, PUT, DELETE, PATCH, OPTIONS");
 
-  $page = $_POST['page'];
-  $per_page = $_POST['per_page'] ?? 5;
-  $offset = $_POST['offset'] ?? 0;
-  $mode = $_POST['mode'] ?? "classic";
+    $page = $_POST['page'];
+    $per_page = $_POST['per_page'] ?? 5;
+    $offset = $_POST['offset'] ?? 0;
+    $mode = $_POST['mode'] ?? "classic";
 
-  if(preg_match("#[^0-9]#", $page)) {
-    echo json_encode($mode === "classic" ? []:_EMPTY_DATA_);
-    return;
-  }
-
-  $page = intval($page);
-
-  $query = new WP_Query([
-    'post_type' => ["post"],
-    'post_status' => ["publish"],
-    'posts_per_page' => $per_page,
-    'offset' => ($per_page * ( $page -1 )) + $offset
-  ]);
-
-  if($query->have_posts()) {
-    $stack = [];
-
-    while($query->have_posts()) {
-      $query->the_post();
-
-      global $post;
-
-      $stack[] = [
-        'id' => $post->ID,
-        'slug' => $post->post_name,
-        'title' => $post->post_title,
-        'excerpt' => $post->post_excerpt,
-        'date' => $post->post_date
-      ];
+    foreach([$page, $per_page, $offset] as $item) {
+        if (preg_match("#[^0-9]#", $item)) {
+            echo json_encode($mode === "classic" ? [] : _EMPTY_DATA_);
+            exit;
+        }
     }
 
-    wp_reset_postdata();
+    $page = intval($page);
+    $per_page = intval($per_page);
+    $offset = intval($offset);
+    $sql_offset = ($per_page * ($page - 1)) + $offset;
+
+    global $wpdb;
+    $result = $wpdb->get_results("select ID, post_name, post_title, post_excerpt, post_date from $wpdb->posts where post_type='post' and post_status='publish' limit $sql_offset, $per_page", ARRAY_A);
+
+    $stack = [];
+    $count = 0;
+    foreach($result as $post) {
+        $count++;
+        $stack[] = [
+            'id' => $post['ID'],
+            'slug' => $post['post_name'],
+            'title' => $post['post_title'],
+            'excerpt' => $post['post_excerpt'],
+            'date' => $post['post_date']
+        ];
+    }
 
     header("Content-Type: application/json");
 
     echo json_encode([
-      'status' => "OK",
-      'content' => $mode === "classic" ? $stack:[
-          'total' => $query->found_posts,
-          'list' => $stack
-      ]
+        'status' => "OK",
+        'content' => $mode === "classic" ? $stack : [
+            'total' => $count,
+            'list' => $stack
+        ]
     ]);
-  } else {
-    echo json_encode($mode === "classic" ? []:_EMPTY_DATA_);
-  }
 
-  exit;
+    exit;
 }
 
 add_action("wp_ajax_articles", "load_articles");
